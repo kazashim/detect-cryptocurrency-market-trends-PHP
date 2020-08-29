47c7705cfc37d274d173be897b831a0872ab0ee7
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-08-29 15:33:31 +0200

    Update example.php

--- example.php
+++ example.php
@@ -1,3 +1,17 @@
 <?php
 
+/*
+|Author: Kazashim Kuzasuwat
+|Title: Detect Cryptocurrency Market Trends Using PHP 
+|Date: 29-08-2020
+*/
 $response = json_decode(file_get_contents('https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=60'));
+
+$prices = [];
+
+foreach ($response->Data as $v) {
+    $prices[] = $v->close;
+}
+
+$ema8 = trader_ema($prices, 8);
+$ema21 = trader_ema($prices, 21);
